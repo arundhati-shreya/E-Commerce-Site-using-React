adceac9897997acf14c59506291dfcc69e4a785a
arundhati-shreya <arundhati0426@gmail.com>
2024-01-04 17:09:04 +0530

    Adding new Movies intio the list

--- fetch-movies/src/App.js
+++ fetch-movies/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -76,6 +77,7 @@ function App() {
 
   return (
     <React.Fragment>
+      <section> <Form/></section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
