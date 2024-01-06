69ca9d5c7c7be4af60e0fe3de49399bafb4d7928
arundhati-shreya <arundhati0426@gmail.com>
2024-01-07 03:39:35 +0530

    Protecting the product page

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,5 +1,5 @@
-import React, { useContext,useState } from "react";
-import {Link} from 'react-router-dom'
+import React, { useContext, useState } from "react";
+import { Link } from 'react-router-dom'
 
 import Cart from "../Cart/Cart";
 import { CartContext } from "../Store/ContextProvider";
@@ -8,40 +8,40 @@ import AuthContext from "../Store/auth-context";
 
 const Header = () => {
 
-    const { cartItemCount,setCartItemCount } = useContext(CartContext);
-    const[show,setShow]=useState(false);
+    const { cartItemCount, setCartItemCount } = useContext(CartContext);
+    const [show, setShow] = useState(false);
     const authCtx = useContext(AuthContext);
-    
+   
+
     const isLoggedIn = authCtx.isLoggedIn;
 
-    const showHandle=()=>{
+    const showHandle = () => {
         setShow(!show)
     }
 
     const updateCartItemCount = (count) => {
-       setCartItemCount(count);
+        setCartItemCount(count);
     };
 
-   
 
     return (
         <>
-           <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+            <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
                 <ul className="nav  me-3">
                     <li className="nav-item">
                         <Link className="nav-link active text-white fw-bold" aria-current="page" to="/home">HOME</Link>
                     </li>
-                    {isLoggedIn && 
-                    <li className="nav-item">
-                        <Link className="nav-link text-white fw-bold" to="/">STORE</Link>
-                    </li>}
+
+                     <li className="nav-item">
+                        <Link className="nav-link text-white fw-bold" to={isLoggedIn ? "/" : "/login"}> STORE</Link>
+                    </li>
                     <li className="nav-item">
                         <Link className="nav-link text-white fw-bold" to="/about">ABOUT</Link>
                     </li>
                     {!isLoggedIn &&
-                    <li className="nav-item">
-                        <Link className="nav-link text-white fw-bold" to="/login">LOGIN</Link>
-                    </li>}
+                        <li className="nav-item">
+                            <Link className="nav-link text-white fw-bold" to="/login">LOGIN</Link>
+                        </li>}
                     <li className="nav-item">
                         <Link className="nav-link text-white fw-bold" to="/contact">CONTACT US</Link>
                     </li>
@@ -49,14 +49,14 @@ const Header = () => {
                 <button type="button" className="btn btn-outline-info ms-auto me-3" onClick={showHandle}>
                     Cart {cartItemCount}
                     {/* {cartItemCount > 0 && <span className="badge bg-danger ms-1">{cartItemCount}</span>} */}
-                    </button>
-                
+                </button>
+
             </nav>
             <div className="bg-secondary text-white text-center py-4">
                 <h1 className="display-4 fw-bold">The Generics</h1>
             </div>
             {show && <Cart updateCartItemCount={updateCartItemCount} />}
-           
+
         </>
     )
 }
